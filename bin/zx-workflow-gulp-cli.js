8a905383a38a249bfb536a3e18f2498b1fbdcf5b
zxwin0125 <zxwin_0125@163.com>
2024-12-04 16:33:53 +0800

    fix: gulp es

--- bin/zx-workflow-gulp-cli.js
+++ bin/zx-workflow-gulp-cli.js
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
-const path = require('path');
+import { spawn } from 'child_process';
+import path from 'path';
 
 // 获取命令行参数
 const args = process.argv.slice(2);
 
 // 添加必要的参数
 args.push('--cwd', process.cwd());
-args.push('--gulpfile', require.resolve('../lib/index.js'));
+args.push('--gulpfile', path.resolve(__dirname, '../lib/index.js'));
 
-// 打印最终的参数列表以供调试（可选）
-// console.log('Final arguments:', args);
+console.log('Final arguments:', args);
 
 // 启动子进程来运行 Gulp
 const gulpProcess = spawn('npx', ['gulp', ...args], {
-  stdio: 'inherit', // 继承父进程的标准输入输出流
+  stdio: 'inherit',
 });
 
 // 监听子进程关闭事件，以便正确退出
