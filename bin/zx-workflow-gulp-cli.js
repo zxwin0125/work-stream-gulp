d909670f6714bd886dbbf1fa82fac6d1ccf8b963
zxwin0125 <zxwin_0125@163.com>
2024-12-04 15:13:38 +0800

    fix: cli

--- bin/zx-workflow-gulp-cli.js
+++ bin/zx-workflow-gulp-cli.js
@@ -4,14 +4,14 @@ const { spawn } = require('child_process');
 const path = require('path');
 
 // 构造 Gulp 命令及参数
-const gulpPath = require.resolve('gulp/bin/gulp');
 const args = [
+  'gulp',
   '--cwd', process.cwd(),
-  '--gulpfile', path.resolve(require.resolve('..'), '../Gulpfile.js')
+  '--gulpfile', path.resolve(__dirname, '../Gulpfile.js')
 ].concat(process.argv.slice(2)); // 添加用户传递的额外参数
 
 // 启动子进程来运行 Gulp
-const gulpProcess = spawn(gulpPath, args, {
+const gulpProcess = spawn('npx', args, {
   stdio: 'inherit', // 继承父进程的标准输入输出流
 });
 
